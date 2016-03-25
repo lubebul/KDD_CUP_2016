d0c587504d71cb1c19d9b1dc494a4049fe4081db
= <=>
2016-03-25 18:47:17 +0800

    fix plot bug

--- OMNI.py
+++ OMNI.py
@@ -68,7 +68,7 @@ class PrepareGraph:
         S = snap.TIntStrH()
         for i in range(self.N):
             for j in range(i+1, self.N):
-                for x in range(1,int(graph[i,j])):
+                for x in range(0,int(graph[i,j])):
                     G.AddEdge(i,j)
             S.AddDat(i, self.authorNameDict[i])
         snap.DrawGViz(G, snap.gvlDot, outfile, "Graph", S)
@@ -176,24 +176,24 @@ class OMNIProp:
             TT[j,:] = upper/down
         return TT
 
-Confs = ['SIGIR', 'SIGMOD', 'SIGCOMM']
-for confName in Confs:
-    omni = OMNIProp(confName, lamda=1.0, eta=0.3)
-    (AS,AT,CS,CT) = omni.run(2011,2015)
-
-    data = {}
-    for i in range(11):
-        data['acceptNum_self_{0}'.format(i)] = AS[:,i]
-        data['acceptNum_t_{0}'.format(i)] = AT[:,i]
-        data['coAuthorNum_self_{0}'.format(i+1)] = CS[:,i]
-        data['coAuthorNum_t_{0}'.format(i+1)] = CT[:,i]
-    data['Author_ID'] = pd.Series([omni.pre.authorIdDict[x] for x in range(omni.N)])
-    data['Author_Name'] = pd.Series([omni.pre.authorNameDict[x] for x in range(omni.N)])
-    df = pd.DataFrame(data)
-    df.to_pickle('OMNI_result_{0}.pkl'.format(omni.pre.confId))
-
-# plot graph
 # Confs = ['SIGIR', 'SIGMOD', 'SIGCOMM']
 # for confName in Confs:
 #     omni = OMNIProp(confName, lamda=1.0, eta=0.3)
-#     omni.pre.plotGraph(omni.graph, '{0}.gif'.format(confName))
+#     (AS,AT,CS,CT) = omni.run(2011,2015)
+
+#     data = {}
+#     for i in range(11):
+#         data['acceptNum_self_{0}'.format(i)] = AS[:,i]
+#         data['acceptNum_t_{0}'.format(i)] = AT[:,i]
+#         data['coAuthorNum_self_{0}'.format(i+1)] = CS[:,i]
+#         data['coAuthorNum_t_{0}'.format(i+1)] = CT[:,i]
+#     data['Author_ID'] = pd.Series([omni.pre.authorIdDict[x] for x in range(omni.N)])
+#     data['Author_Name'] = pd.Series([omni.pre.authorNameDict[x] for x in range(omni.N)])
+#     df = pd.DataFrame(data)
+#     df.to_pickle('OMNI_result_{0}.pkl'.format(omni.pre.confId))
+
+# plot graph
+Confs = ['SIGIR', 'SIGMOD', 'SIGCOMM']
+for confName in Confs:
+    omni = OMNIProp(confName, lamda=1.0, eta=0.3)
+    omni.pre.plotGraph(omni.graph, '{0}.gif'.format(confName))
